58fa629e277d5bbb8279129aebd7088b95507a52
Hau Nguyen <paulnguyen.mn@gmail.com>
2020-11-27 23:56:05 +0700

    08 - Nested routings

--- src/features/Home/index.jsx
+++ src/features/Home/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Introduction from './components/Introduction';
 
 HomePage.propTypes = {};
 
 function HomePage(props) {
-  return <div>HOME PAGE</div>;
+  return (
+    <div>
+      HOME PAGE
+      <Introduction />
+    </div>
+  );
 }
 
 export default HomePage;
