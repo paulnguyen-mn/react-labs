ce5bb163d917277bbeb1b83bcdba020715fc9c2e
Hau Nguyen <paulnguyen.mn@gmail.com>
2020-12-19 08:40:39 +0700

    16 - React Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import ThemeContext, { themes } from 'themeContext';
 import './App.scss';
 import Header from './components/Header';
 import NotFound from './components/NotFound';
@@ -9,20 +11,25 @@ import StudentFeature from './features/Student';
 import TodoFeature from './features/Todo';
 
 function App() {
+  const [currentTheme, setCurrentTheme] = useState(themes.light);
+  const value = { currentTheme, setCurrentTheme };
+
   return (
     <div>
-      <Header />
+      <ThemeContext.Provider value={value}>
+        <Header />
 
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/box" component={MagicBoxFeature} />
-        <Route path="/rendering" component={RenderingFeature} />
-        <Route path="/students" component={StudentFeature} />
-        <Route path="/todos" component={TodoFeature} />
-        <Route component={NotFound} />
-      </Switch>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/box" component={MagicBoxFeature} />
+          <Route path="/rendering" component={RenderingFeature} />
+          <Route path="/students" component={StudentFeature} />
+          <Route path="/todos" component={TodoFeature} />
+          <Route component={NotFound} />
+        </Switch>
 
-      <div style={{ textAlign: 'center' }}>Created by Po with ❤️</div>
+        <div style={{ textAlign: 'center' }}>Created by Po with ❤️</div>
+      </ThemeContext.Provider>
     </div>
   );
 }
