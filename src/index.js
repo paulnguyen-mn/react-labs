87eeb30bf40dd336a43acb20235c259cc80a2a0e
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-01-12 09:19:43 +0700

    Setup i18n

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 import { CssBaseline } from '@material-ui/core';
 import store from 'app/store';
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+
+import './i18n';
+import NotFound from 'components/NotFound';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <CssBaseline />
-        <App />
+        <Suspense fallback={<div>Loading ...</div>}>
+          <Switch>
+            <Redirect exact from="/" to="/en" />
+
+            <Route path="/:lng" component={App} />
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
